94ebbee5b00a41a4cf0772461b66adc5b693fe9d
Buras26 <buras.hovhannisyan@gmail.com>
2026-06-27 15:38:47 +0400

    utils

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -24,3 +24,59 @@ def assign_season(month_number):
         return "Summer"
     else:
         return "Autumn"
+    
+def geojson_downloader(url:str, name:str, path:str)->gpd.GeoDataFrame:
+    df = gpd.read_file(url)
+    df.to_file(f'{path}/{name}.geojson'),
+    driver='GeoJSON'
+    print(f'The {name}.geojson was successfully stored in {path}')
+    return df
+
+
+
+def create_neighborhood_choropleth(
+    gdf,
+    metric,
+    legend_name,
+    neighborhood_col="neighborhood"
+):
+    choropleth_map = folium.Map(
+        location=[center_lat, center_lng],
+        zoom_start=12
+    )
+
+    folium.Choropleth(
+        geo_data=gdf,
+        data=gdf,
+        columns=[neighborhood_col, metric],
+        key_on=f"feature.properties.{neighborhood_col}",
+        fill_opacity=0.7,
+        line_opacity=0.4,
+        legend_name=legend_name,
+        nan_fill_opacity=0.1
+    ).add_to(choropleth_map)
+
+    folium.GeoJson(
+        gdf,
+        name="Neighborhood Boundaries",
+        tooltip=folium.GeoJsonTooltip(
+            fields=[
+                neighborhood_col,
+                metric
+            ],
+            aliases=[
+                "Neighborhood:",
+                f"{legend_name}:"
+            ],
+            localize=True
+        ),
+        style_function=lambda feature: {
+            "fillOpacity": 0,
+            "color": "black",
+            "weight": 1
+        }
+    ).add_to(choropleth_map)
+
+    folium.LayerControl().add_to(choropleth_map)
+
+    return choropleth_map
